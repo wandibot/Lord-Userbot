9955b795acab3b2f17f5a8d903b6adafe523cfa9
ZEEONE <78480148+wandibot@users.noreply.github.com>
2021-05-12 15:47:22 +0800

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -61,7 +61,7 @@ async def help(lord):
                         "`transform`  `update`  `download`  `getid`  `waifu`  `wallpaper`  `weather`\n\n"
                         "**⚡️ MODULES 15:**\n"
                         "`webupload`  `welcome`  `whois`  `ping`  `sinyal`  `xiaomi`  `zipfile`")
-        await lord.reply("\n**CARA MENGGUNAKAN,** **CONTOH:**\n**KETIK** `.help afk` **UNTUK INFORMASI MODULES**\n**GROUP SUPPORT:** [TEKAN](t.me/LordUserbot_Group)")
+        await lord.reply("\n**CARA MENGGUNAKAN,** **CONTOH:**\n**KETIK** `.help afk` **UNTUK INFORMASI MODULES**\n**GROUP SUPPORT:** [TEKAN](t.me/zeeone_ofc)")
         await asyncio.sleep(1000)
         await lord.delete()
 
